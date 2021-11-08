bdeb9729c9392bd6a5ecbe8479a34bed69c15a22
Agrima <qvistn111@gmail.com>
2021-11-08 11:36:50 +0530

    Space changed

--- hello-world.c
+++ hello-world.c
@@ -10,8 +10,6 @@ int main()
     scanf("%[^\n]%*c", &s);
     
     printf("Hello, World!\n");
-    printf(s);
-  	
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    printf(s);    
     return 0;
 }
